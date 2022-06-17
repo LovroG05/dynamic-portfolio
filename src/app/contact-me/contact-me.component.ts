b0e349150c94e9bea081252b8e426ed64e68e101
LovroG05 <lovro.govekar@gmail.com>
2022-06-18 00:52:26 +0200

    done?

---
+++ src/app/contact-me/contact-me.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-me',
+  templateUrl: './contact-me.component.html',
+  styleUrls: ['./contact-me.component.scss']
+})
+export class ContactMeComponent {
+
+}
